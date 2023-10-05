c693f34eb195d5a87cbe598902b9bc8563b806a4
jhoel debray quispe ticona <jhoel.quispe@ficct.uagrm.edu.bo>
2023-10-05 19:37:09 -0400

    commit pal compa

---
+++ resources/views/examen.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <img src="{{ asset('/vendor/1.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/2.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/3.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/4.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/5.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/6.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/7.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/8.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/9.jpeg') }}" alt="Descripción de la imagen">
+    <img src="{{ asset('/vendor/10.jpeg') }}" alt="Descripción de la imagen">
+
+
+
+
+
+</body>
+</html>
